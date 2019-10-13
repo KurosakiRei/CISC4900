66d87c199d0e988e510bf19b77d0e21efe33fb63
KurosakiRei <KurosakiRei@users.noreply.github.com>
2019-10-13 17:39:06 -0400

    SDK samples

---
+++ AWS sample.py
@@ -0,0 +1,16 @@
+import boto3
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+image = get_file_content('hashiqi.jpg')
+
+client = boto3.client('rekognition')
+response = client.detect_labels(
+    Image={
+        'Bytes': image,
+        }
+    
+)
+print(response)
---
+++ IBM sample.py
@@ -0,0 +1,15 @@
+import json
+from ibm_watson import VisualRecognitionV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('si21GLXqCMSdhw15pibaI9Dl98v-n_IrO1-_NNfSk95B')
+visual_recognition = VisualRecognitionV3(
+    version='2018-03-19',
+    authenticator=authenticator
+)
+
+with open('hashiqi.jpg', 'rb') as images_file:
+    classes = visual_recognition.classify(
+        images_file=images_file,
+        threshold='0.6').get_result()
+    print(json.dumps(classes, indent=2))
