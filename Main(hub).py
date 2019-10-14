1f37dc0f06539e1f13a3ee0448e9f62bc997fadd
KurosakiRei <KurosakiRei@users.noreply.github.com>
2019-10-14 01:53:25 -0400

    Update Main(hub).py

--- Main(hub).py
+++ Main(hub).py
@@ -4,7 +4,7 @@ import vendors.Google as Google
 import vendors.IBM as IBM
 
 if __name__ == '__main__':
-    aws = AWS.AWS()
-    clarifai = Clarifai.Clarifai()
-    google = 
-    print(aws.image_recognition('hashiqi.jpg'))
+    aws = AWS.Rekonition()
+    clarifai = Clarifai.Predict()
+    google = Google.VisionAI()
+    ibm = IBM.WatsonVisualRecognition()
