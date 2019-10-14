5818faf328e7fdf6354637f1aaded8c27ce57ecc
KurosakiRei <KurosakiRei@users.noreply.github.com>
2019-10-14 01:35:18 -0400

    Fix and update Main class

--- Main(hub).py
+++ Main(hub).py
@@ -1,4 +1,8 @@
-from .vendors import AWS_Class
+import vendors.AWS as AWS
+import vendors.Clarifal as Clarifal
+import vendors.Google as Google
+import vendors.IBM as IBM
 
 if __name__ == '__main__':
-    aws = AWS_Class.AWS()
+    aws = AWS.AWS()
+    print(aws.image_recognition('hashiqi.jpg'))
