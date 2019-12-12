6e81800fae6d3e2f4097e32393be8ba0e75d86a0
KurosakiRei <KurosakiRei@users.noreply.github.com>
2019-12-11 22:30:16 -0500

    config file

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+CLARIFAI = {'api_key':'', 'version':''} # Change the values to your api_key and version
+IBM = {'api_key':'', 'version':''} # Change the values to your api_key and version
+GOOGLE = os.pardir + os.sep + 'GoogleApi.json' # Change the path to the directory of your google api_key json file 
+AMAZON = # %UserProfile%\.aws is the default path for credentials file and config file
